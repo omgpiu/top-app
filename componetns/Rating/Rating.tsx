238b3d6aa457a398ac81b303138f585f04855472
omgpiu <dyba@list.ru>
2021-07-11 13:38:09 +0300

    Fix: CSS pointer without spaces

--- componetns/Rating/Rating.tsx
+++ componetns/Rating/Rating.tsx
@@ -18,17 +18,20 @@ export const Rating: React.FC<RatingProps> = ({
     const constructRating = (currentRating: number) => {
         const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
             return (
-                <StarIcon
+                <span
                     className={ cn(RatingCSS.star, {
                         [RatingCSS.filled]: i < currentRating,
                         [RatingCSS.editable]: isEditable
                     }) }
                     onMouseEnter={ () => changeDisplay(i + 1) }
                     onMouseLeave={ () => changeDisplay(rating) }
-                    onClick={ () => changeRating(i + 1) }
-                    tabIndex={ isEditable ? 0 : -1 }
-                    onKeyDown={ (e: KeyboardEvent<SVGElement>) => isEditable && addBySpace(i + 1, e) }
-                />
+                    onClick={ () => changeRating(i + 1) }>
+                     <StarIcon
+                         tabIndex={ isEditable ? 0 : -1 }
+                         onKeyDown={ (e: KeyboardEvent<SVGElement>) => isEditable && addBySpace(i + 1, e) }
+                     />
+                </span>
+
             )
         })
         setRatingArray(updatedArray)
