9ffdad327d63bbb772d0c4fe8ea0836ff387f89d
omgpiu <dyba@List.ru>
2021-07-15 21:05:31 +0300

    Css for menu/add icons

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../../context';
+import Link from 'next/link';
 import st from './Menu.module.css';
 import { FirstLevelMenuItem, IPageItem } from '../../interfaces/menu.interface';
 import { TopLevelCategory } from '../../interfaces/top-page.interface';
@@ -7,28 +8,31 @@ import { Book, Box, Cloud, Hat } from './icons';
 import cn from 'classnames';
 
 const firstLevelMenu: FirstLevelMenuItem[] = [
-  {id: TopLevelCategory.Courses, title: 'Курсы', icon: <Hat />, route: '/courses'},
-  {id: TopLevelCategory.Services, title: 'Сервисы', icon: <Cloud />, route: '/services'},
-  {id: TopLevelCategory.Books, title: 'Книги', icon: <Book />, route: '/books'},
-  {id: TopLevelCategory.Products, title: 'Товары', icon: <Box />, route: '/products'},
+  {id: TopLevelCategory.Courses, title: 'Курсы', icon: <Hat />, route: 'courses'},
+  {id: TopLevelCategory.Services, title: 'Сервисы', icon: <Cloud />, route: 'services'},
+  {id: TopLevelCategory.Books, title: 'Книги', icon: <Book />, route: 'books'},
+  {id: TopLevelCategory.Products, title: 'Товары', icon: <Box />, route: 'products'},
 
 
 ];
 
 export const Menu: React.FC = (): JSX.Element => {
   const {menu, setMenu, firstCategory} = useContext(AppContext);
+  const [isOpen, setIsOpen] = useState(false);
   const firstLevelMenuRender = () => {
     return (
       <>
         {firstLevelMenu.map(flmenu => (
           <div key={flmenu.route}>
-            <a href={`/${flmenu.route}`}>
-              <div className={cn(st.firstLevel, {
-                [st.firstLevelActive]: flmenu.id === firstCategory
-              })}>
-                {flmenu.icon}<span>{flmenu.title}</span>
-              </div>
-            </a>
+            <Link href={`/${flmenu.route}`}>
+              <a>
+                <div className={cn(st.firstLevel, {
+                  [st.firstLevelActive]: flmenu.id === firstCategory
+                })}>
+                  {flmenu.icon}<span>{flmenu.title}</span>
+                </div>
+              </a>
+            </Link>
             {
               flmenu.id === firstCategory && secondLevelMenuRender(flmenu.route)
             }
@@ -40,7 +44,7 @@ export const Menu: React.FC = (): JSX.Element => {
   };
   const secondLevelMenuRender = (route: string) => {
     return (
-      <>
+      <div className={st.secondBlock} onClick={() => setIsOpen(prev => !prev)}>
         {menu.map(slmenu => (
           <div key={slmenu._id.secondCategory}>
             <div className={st.secondLevel}>
@@ -55,23 +59,27 @@ export const Menu: React.FC = (): JSX.Element => {
             </div>
           </div>
         ))}
-      </>
+      </div>
     );
   };
   const thirdLevelMenuRender = (pages: IPageItem[], route: string) => {
     return (
-      pages.map(page => (
-        <a href={`/${route}/${page.alias}`}
-           className={cn(st.thirdLevel, {
-             [st.thirdLevelActive]: true
-           })}
+      <>
+        {pages.map(page => (
+          <Link key={page.alias + page.category} href={`/${route}/${page.alias}`}>
+            <a
+              className={cn(st.thirdLevel, {
+                [st.thirdLevelActive]: false
+              })}
 
-        >{page.category}</a>
-
-
-      ))
+            >{isOpen && page.category}</a>
+          </Link>
+        ))}
+      </>
     );
   };
+
+
   return (
     <div className={st.wrapper}>
       {firstLevelMenuRender()}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,22 @@
-import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(context)
-        return { ...initialProps }
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    }
-
-    render(): JSX.Element {
-        return (
-            <Html lang={ 'ru' }>
-                <Head><title>E-commerce</title></Head>
-                <body>
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
-        )
-    }
+  render(): JSX.Element {
+    return (
+      <Html lang="ru">
+        <Head />
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
