9b287302576e693ec7d3223490be344a2729cad2
omgpiu <dyba@List.ru>
2021-10-11 17:55:37 +0300

    Menu animation

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -7,93 +7,130 @@ import { FirstLevelMenuItem, IPageItem } from '../../interfaces/menu.interface';
 import cn from 'classnames';
 import { useRouter } from 'next/router';
 import { firstLevelMenu } from '../../helpers';
-
+import { motion } from 'framer-motion';
 
 
 export const Menu: React.FC = (): JSX.Element => {
-    const { menu, setMenu, firstCategory } = useContext(AppContext);
-    const router = useRouter();
 
-    const openSecondLevel = (secondCategory: string) => () => {
-        setMenu && setMenu(menu.map(m => {
-            if (m._id.secondCategory === secondCategory) {
-                m.isOpen = !m.isOpen;
-            }
-            return m;
-        }));
+  const router = useRouter();
 
-    };
+  const {menu, setMenu, firstCategory} = useContext(AppContext);
 
-    const firstLevelMenuRender = () => {
-        return (
-            <>
-                { firstLevelMenu.map(flmenu => (
-                    <div key={ flmenu.route }>
-                        <Link href={ `/${ flmenu.route }` }>
-                            <a>
-                                <div className={ cn(st.firstLevel, {
-                                    [st.firstLevelActive]: flmenu.id == firstCategory
-                                }) }>
-                                    { flmenu.icon }
-                                    <span>{ flmenu.title }</span>
-                                </div>
-                            </a>
-                        </Link>
-                        {
-                            flmenu.id === firstCategory && secondLevelMenuRender(flmenu)
-                        }
-
-                    </div>
-                )) }
-            </>
-        );
-    };
-    const secondLevelMenuRender = (flmenu: FirstLevelMenuItem) => {
-        return (
-            <div className={ st.secondBlock }>
-                { menu.map(slmenu => {
-                    if (slmenu.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
-
-                        slmenu.isOpen = true;
-                    }
-                    return (
-                        <div key={ slmenu._id.secondCategory }>
-                            <div className={ st.secondLevel } onClick={ openSecondLevel(slmenu._id.secondCategory) }>
-                                { slmenu._id.secondCategory }
-                            </div>
-                            <div className={ cn(st.secondLevelBlock, {
-                                [st.secondLevelBlockOpen]: slmenu.isOpen
-                            }) }>
-                                { thirdLevelMenuRender(slmenu.pages, flmenu.route) }
-                            </div>
-                        </div>
-                    );
-                }) }
-            </div>
-        );
-    };
-    const thirdLevelMenuRender = (pages: IPageItem[], route: string) => {
-        return (
-            <>
-                { pages.map(page => (
-                    <Link key={ page.alias + page.category } href={ `/${ route }/${ page.alias }` }>
-                        <a
-                            className={ cn(st.thirdLevel, {
-                                [st.thirdLevelActive]:  `/${route}/${page.alias}` == router.asPath
-                            }) }
-
-                        > { page.category }</a>
-                    </Link>
-                )) }
-            </>
-        );
-    };
+  const variantsSecondLevel = {
+    visible: {
+      marginBottom: 20,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.1
+      }
+    },
+    hidden: {
+      marginBottom: 0
+    }
+  };
+  const variantsThirdLevel = {
+    visible: {
+      opacity: 1,
+      //высота элемента
+      height: 29,
+    },
+    hidden: {
+      opacity: 0,
+      height: 0,
+    }
+  };
 
 
+  const openSecondLevel = (secondCategory: string) => () => {
+    setMenu && setMenu(menu.map(m => {
+      if (m._id.secondCategory === secondCategory) {
+        m.isOpen = !m.isOpen;
+      }
+      return m;
+    }));
+
+  };
+
+  const firstLevelMenuRender = () => {
     return (
-        <div className={ st.wrapper }>
-            { firstLevelMenuRender() }
-        </div>
+      <>
+        {firstLevelMenu.map(flmenu => (
+          <div key={flmenu.route}>
+            <Link href={`/${flmenu.route}`}>
+              <a>
+                <div className={cn(st.firstLevel, {
+                  [st.firstLevelActive]: flmenu.id == firstCategory
+                })}>
+                  {flmenu.icon}
+                  <span>{flmenu.title}</span>
+                </div>
+              </a>
+            </Link>
+            {
+              flmenu.id === firstCategory && secondLevelMenuRender(flmenu)
+            }
 
+          </div>
+        ))}
+      </>
     );
+  };
+  const secondLevelMenuRender = (flmenu: FirstLevelMenuItem) => {
+    return (
+      <div className={st.secondBlock}>
+        {menu.map(slmenu => {
+          if (slmenu.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
+
+            slmenu.isOpen = true;
+          }
+          return (
+            <div key={slmenu._id.secondCategory}>
+              <div className={st.secondLevel} onClick={openSecondLevel(slmenu._id.secondCategory)}>
+                {slmenu._id.secondCategory}
+              </div>
+              <motion.div
+                layout
+                variants={variantsSecondLevel}
+                initial={slmenu.isOpen ? 'visible' : 'hidden'}
+                animate={slmenu.isOpen ? 'visible' : 'hidden'}
+                className={st.secondLevelBlock}
+              >
+                {thirdLevelMenuRender(slmenu.pages, flmenu.route)}
+              </motion.div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+  const thirdLevelMenuRender = (pages: IPageItem[], route: string) => {
+    return (
+      <>
+        {pages.map(page => (
+          <motion.div key={page.alias + page.category}
+                      variants={variantsThirdLevel}
+            // initial={'hidden'}
+
+          >
+            <Link href={`/${route}/${page.alias}`}>
+              <a
+                className={cn(st.thirdLevel, {
+                  [st.thirdLevelActive]: `/${route}/${page.alias}` == router.asPath
+                })}
+
+              > {page.category}</a>
+            </Link>
+          </motion.div>
+        ))}
+      </>
+    );
+  };
+
+
+  return (
+    <div className={st.wrapper}>
+      {firstLevelMenuRender()}
+    </div>
+
+  );
 };
